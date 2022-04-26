68ad0c4de2f7856cbcadf42fbc415aa2b4aa3cbd
Harsh Tomar <harshtomar67tb@gmail.com>
2022-04-26 16:59:33 +0530

    style: Add theme
    3 themes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // styles
 import './App.scss'
 
+import { useState } from 'react'
+
 const keys = [
   '7',
   '8',
@@ -23,59 +25,72 @@ const keys = [
 ]
 
 function App() {
+
+  const [theme, setTheme] = useState('theme_1')
+
+  const changeTheme = () => {
+    let newTheme = theme == 'theme_1' ? 'theme_2' : (theme == 'theme_2' ? 'theme_3' : 'theme_1') 
+    
+    setTheme(newTheme)
+  }  
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
 
-      <section className="header">
+      <div className="calculator">
 
-          <span className="title">
-            calc
-          </span>
-        
-          <span className="theme">
-              theme
-          </span>
-        
-          <div className="themeBtn">
+        <section className="header">
 
-              <div 
-                className="themeBtn__container" 
-                // onClick={changeTheme}
-              >
-            
-                  <div className="themeBtn__switch"></div>
-            
-              </div>
+            <span className="title">
+              calc
+            </span>
           
+            <span className="theme">
+                theme
+            </span>
+          
+            <div className="themeBtn">
+
+                <div 
+                  className="themeBtn__container" 
+                  onClick={changeTheme}
+                >
+              
+                    <div className="themeBtn__switch"></div>
+              
+                </div>
+            
+            </div>
+        
+        </section>
+
+        <section className="display">
+          
+          <div>
+        
+              99 {/* {inputStr} */}
+        
           </div>
-      
-      </section>
-
-      <section className="display">
         
-        <div>
-      
-            99 {/* {inputStr} */}
-      
-        </div>
-      
-      </section>
-      
-      <section className="keys">
-
-        {
-          keys.map(key => (
-            <button
-              key={key}
-              className={key=='=' ? 'equal' : key}
-              data-val={key}
-            >
-              {key}
-            </button>
-          ))
-        }
+        </section>
         
-      </section>
+        <section className="keys">
+
+          {
+            keys.map(key => (
+              <button
+                key={key}
+                className={key=='=' ? 'equal' : key}
+                data-val={key}
+              >
+                {key}
+              </button>
+            ))
+          }
+
+        </section>
+
+      </div>
 
     </div>
   )
